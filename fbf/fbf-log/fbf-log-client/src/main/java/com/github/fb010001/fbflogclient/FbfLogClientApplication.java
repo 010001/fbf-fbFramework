355cebd44cab32024d2926852fad70cc51c45e7a
fangbin203 <010001>
2019-12-13 10:28:44 +0800

    增加日志模块

---
+++ fbf/fbf-log/fbf-log-client/src/main/java/com/github/fb010001/fbflogclient/FbfLogClientApplication.java
@@ -0,0 +1,12 @@
+package com.github.fb010001.fbflogclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbfLogClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbfLogClientApplication.class, args);
+    }
+}
---
+++ fbf/fbf-log/fbf-log-service/src/main/java/com/github/fb010001/fbflogservice/FbfLogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.fb010001.fbflogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbfLogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbfLogServiceApplication.class, args);
+    }
+}
