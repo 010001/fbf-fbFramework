3d7446651214003eb9af4d7994c8c19224430cc3
fangbin203 <010001>
2019-12-14 11:19:03 +0800

    增加构建工程和安装脚本工程

---
+++ fbf/fbf-redis-lock/src/main/java/com/github/fb010001/fbfredislock/FbfRedisLockApplication.java
@@ -0,0 +1,12 @@
+package com.github.fb010001.fbfredislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbfRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbfRedisLockApplication.class, args);
+    }
+}
---
+++ fbf/fbf-redis-lock/src/test/java/com/github/fb010001/fbfredislock/FbfRedisLockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.fb010001.fbfredislock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FbfRedisLockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
